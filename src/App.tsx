2a99fefed0186b3afeabd142249e54a1d5419784
Takuto Mori <mr04vv@gmail.com>
2021-06-26 12:42:45 +0900

    chore: チョット変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  useEffect(()=> {
+    const ctx = ref.current?.getContext('2d')
+    if (ctx) {
+      ctx.lineWidth = 30;
+
+      // Wall
+      ctx.strokeRect(75, 140, 150, 110);
+
+      // Door
+      ctx.fillRect(130, 190, 40, 60);
+
+      // Roof
+      ctx.moveTo(50, 140);
+      ctx.lineTo(150, 60);
+      ctx.lineTo(250, 140);
+
+      ctx.closePath();
+      ctx.stroke();
+    }
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas ref={ref} width="1280" height="720" ></canvas>
     </div>
   );
 }
