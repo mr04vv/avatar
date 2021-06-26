1e0a57108347da67a245cf55b2895e98d18f01db
Keita Karei <keitakarei@KeitanoMacBook-Pro.local>
2021-06-26 16:02:06 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -30,9 +30,15 @@ const CANVAS_HEIGHT = 720;
 
 function App() {
   const ref = useRef<HTMLCanvasElement>(null);
-  const [faceLocation, setFaceLocation] = useState<ILocation>(initialLocation);
+  const [mouthLocation, setMouthLocation] =
+    useState<ILocation>(initialLocation);
+  const [noseLocation, setNoseLocation] =
+    useState<ILocation>(initialLocation);
+  const [rightEyeLocation, setRightEyeLocation] =
+    useState<ILocation>(initialLocation);
+  const [leftEyeLocation, setLeftEyeLocation] =
+    useState<ILocation>(initialLocation);
   const [initialized, setInitialized] = useState<boolean>(false);
-  const [faceX, setFaceX] = useState<number>(0);
 
   const faceImg = useMemo(() => new Image(), []);
   faceImg.src = face;
@@ -49,16 +55,37 @@ function App() {
     const ctx = ref.current?.getContext("2d");
 
     if (ctx) {
-      setFaceLocation({
-        x: CANVAS_WIDTH / 5,
-        y: CANVAS_HEIGHT / 5,
-        width: 300,
-        height: 400,
+      setMouthLocation({
+        x: CANVAS_WIDTH / 2 - 10,
+        y: CANVAS_HEIGHT / 2 - 15,
+        width: 30,
+        height: 30,
+      });
+
+      setNoseLocation({
+        x: CANVAS_WIDTH / 2 - 10,
+        y: CANVAS_HEIGHT / 2 - 45,
+        width: 30,
+        height: 30,
+      });
+
+      setRightEyeLocation({
+        x: CANVAS_WIDTH / 2 + 10,
+        y: CANVAS_HEIGHT / 2 - 60,
+        width: 30,
+        height: 30,
+      });
+
+      setLeftEyeLocation({
+        x: CANVAS_WIDTH / 2 - 30,
+        y: CANVAS_HEIGHT / 2 - 60,
+        width: 30,
+        height: 30,
       });
 
       faceImg.onload = () => {
         if (!initialized)
-          ctx.drawImage(faceImg, CANVAS_WIDTH / 5, CANVAS_HEIGHT / 5, 300, 400);
+          ctx.drawImage(faceImg, CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, 300, 400);
         setInitialized(true);
       };
       leftEyeImg.onload = () => {
@@ -67,7 +94,7 @@ function App() {
             leftEyeImg,
             CANVAS_WIDTH / 12,
             CANVAS_HEIGHT / 5,
-            40,
+            30,
             40
           );
       };
@@ -85,8 +112,8 @@ function App() {
         if (!initialized)
           ctx.drawImage(
             mouthImg,
-            CANVAS_WIDTH / 10 + 10,
-            CANVAS_HEIGHT / 4 + 30,
+            CANVAS_WIDTH / 2 - 10,
+            CANVAS_HEIGHT / 2 - 15,
             30,
             30
           );
@@ -101,6 +128,7 @@ function App() {
             30
           );
       };
+
     }
   }, [faceImg, initialized, leftEyeImg, mouthImg, noseImg, rightEyeImg]);
 
@@ -110,50 +138,136 @@ function App() {
       ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
       ctx.drawImage(
         faceImg,
-        faceLocation.x,
-        faceLocation.y,
-        faceLocation.width,
-        faceLocation.height
+        CANVAS_WIDTH / 2 - 150,
+        CANVAS_HEIGHT / 2 - 200,
+        300,
+        400
       );
-      ctx.drawImage(leftEyeImg, CANVAS_WIDTH / 12, CANVAS_HEIGHT / 5, 40, 40);
-      ctx.drawImage(rightEyeImg, CANVAS_WIDTH / 8, CANVAS_HEIGHT / 5, 40, 40);
+      ctx.drawImage(
+        leftEyeImg,
+        leftEyeLocation.x,
+        leftEyeLocation.y,
+        leftEyeLocation.width,
+        leftEyeLocation.height
+      );
+      ctx.drawImage(
+        rightEyeImg,
+        rightEyeLocation.x,
+        rightEyeLocation.y,
+        rightEyeLocation.width,
+        rightEyeLocation.height
+      );
       ctx.drawImage(
         mouthImg,
-        CANVAS_WIDTH / 10 + 10,
-        CANVAS_HEIGHT / 4 + 30,
-        30,
-        30
+        mouthLocation.x,
+        mouthLocation.y,
+        mouthLocation.width,
+        mouthLocation.height
       );
       ctx.drawImage(
         noseImg,
-        CANVAS_WIDTH / 10 + 10,
-        CANVAS_HEIGHT / 4 + 30,
-        30,
-        30
+        noseLocation.x,
+        noseLocation.y,
+        noseLocation.width,
+        noseLocation.height
       );
     }
-  }, [faceImg, faceLocation, leftEyeImg, mouthImg, rightEyeImg, noseImg]);
+  }, [faceImg, mouthLocation, noseLocation, rightEyeLocation, leftEyeLocation, leftEyeImg, mouthImg, rightEyeImg, noseImg]);
 
   useEffect(() => {
     renderImg();
-  }, [faceImg, faceLocation, renderImg]);
+  }, [faceImg, mouthLocation, renderImg]);
 
   return (
     <div className="App">
       <canvas ref={ref} width={CANVAS_WIDTH} height={CANVAS_HEIGHT}></canvas>
+      <Slider
+        min={-10}
+        max={30}
+        setLocation={(value) => {
+          const ctx = ref.current?.getContext("2d");
+          if (ctx) {
+            setMouthLocation((prevState) => ({
+              ...prevState,
+              y: CANVAS_HEIGHT / 2 - (30 + value * 2) / 2,
+              x: CANVAS_WIDTH / 2 - (30 + value * 2) / 2 + 5,
+              height: 30 + value * 2,
+              width: 30 + value * 2,
+            }));
+          }
+        }}
+      />
       <Slider
         min={-50}
         max={50}
         setLocation={(value) => {
           const ctx = ref.current?.getContext("2d");
           if (ctx) {
-            setFaceLocation((prevState) => ({
+            setMouthLocation((prevState) => ({
               ...prevState,
-              x: CANVAS_WIDTH / 5 + value,
+              y: CANVAS_HEIGHT / 2 - 15 - value,
             }));
           }
         }}
       />
+      <Slider
+        min={-10}
+        max={30}
+        setLocation={(value) => {
+          const ctx = ref.current?.getContext("2d");
+          if (ctx) {
+            setNoseLocation((prevState) => ({
+              ...prevState,
+              y: CANVAS_HEIGHT / 2 - (30 + value * 2) / 2,
+              x: CANVAS_WIDTH / 2 - (30 + value * 2) / 2 + 5,
+              height: 30 + value * 2,
+              width: 30 + value * 2,
+            }));
+          }
+        }}
+      />
+      <Slider
+        min={-50}
+        max={50}
+        setLocation={(value) => {
+          const ctx = ref.current?.getContext("2d");
+          if (ctx) {
+            setNoseLocation((prevState) => ({
+              ...prevState,
+              y: CANVAS_HEIGHT / 2 - 15 - value,
+            }));
+          }
+        }}
+      />
+      {/* <Slider
+        min={-10}
+        max={30}
+        setLocation={(value) => {
+          const ctx = ref.current?.getContext("2d");
+          if (ctx) {
+            setRightEyeLocation((prevState) => ({
+              ...prevState,
+              y: CANVAS_HEIGHT / 2 - (30 + value * 2) / 2,
+              x: CANVAS_WIDTH / 2 - (30 + value * 2) / 2 + 5,
+              height: 30 + value * 2,
+              width: 30 + value * 2,
+            }));
+          }
+        }}
+      />
+      <Slider
+        min={-50}
+        max={50}
+        setLocation={(value) => {
+          const ctx = ref.current?.getContext("2d");
+          if (ctx) {
+            setRightEyeLocation((prevState) => ({
+              ...prevState,
+              y: CANVAS_HEIGHT / 2 - 15 - value,
+            }));
+          }
+        }}
+      /> */}
     </div>
   );
 }
