12c12c7fc723106bdb4d001756b11c3840a12c44
Takuto Mori <mr04vv@gmail.com>
2021-08-24 18:54:24 +0900

    chore: コード整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import face from "./assets/face.svg";
 import nose from "./assets/nose.svg";
 import mouth from "./assets/mouth.svg";
 import eye from "./assets/eye.svg";
-import heart from "./assets/heart.svg";
 import "./App.css";
 import { Slider } from "./slider";
-import { Stage, Sprite, InteractionEvent } from "react-pixi-fiber";
+import { Stage, Sprite } from "react-pixi-fiber";
 import * as PIXI from "pixi.js";
-import axios from "axios";
 
 interface ILocation {
   x: number;
@@ -23,19 +21,14 @@ const initialLocation: ILocation = {
 // const a = PIXI.Texture.from("assets/image.png");
 const CANVAS_WIDTH = 580;
 const CANVAS_HEIGHT = 720;
-const INIT_MOUSE_HIGHT = 20 + 50;
-const INIT_EYE_HEIGHT = -90 + 50;
-const INIT_NOSE_HIGHT = -50 + 50;
+const INIT_MOUSE_HIGHT = 20;
+const INIT_EYE_HEIGHT = -90;
+const INIT_NOSE_HIGHT = -50;
 const INIT_EYE_WIDTH = (isLeft: boolean) =>
   CANVAS_WIDTH / 2 + (isLeft ? -40 : +40);
 const centerAnchor = new PIXI.Point(0.5, 0.5);
 
-const canvasElement: HTMLCanvasElement = document.getElementById(
-  "hoge"
-) as HTMLCanvasElement;
-
 function App() {
-  const ref = useRef<HTMLCanvasElement>(null);
   const [faceLocation, setFaceLocation] = useState<ILocation>(initialLocation);
   const [mouthLocation, setMouthLocation] =
     useState<ILocation>(initialLocation);
@@ -44,14 +37,12 @@ function App() {
     useState<ILocation>(initialLocation);
   const [rightEyeLocation, setRightEyeLocation] =
     useState<ILocation>(initialLocation);
-  const [heartLocation, setHeartLocation] =
-    useState<ILocation>(initialLocation);
 
   useEffect(() => {
     setFaceLocation({
       x: CANVAS_WIDTH / 2,
       y: CANVAS_HEIGHT / 2,
-      scale: 0.6,
+      scale: 0.5,
     });
 
     setMouthLocation({
@@ -79,33 +70,13 @@ function App() {
     });
   }, []);
 
-  const stageRef = React.useRef<HTMLCanvasElement>(null);
-  const [tex, setTex] = useState<PIXI.Texture<PIXI.Resource>>();
-  const [eyetex, setEyeTex] = useState<PIXI.Texture<PIXI.Resource>>();
-  const [sv, setSv] = useState<string>("");
-  const [mouth, setMouth] = useState<string>("");
-
   useEffect(() => {
     (async () => {
+      // cdn経由でsvgを取得して表示するには
       // const res = await axios.get("url");
       // const svgStr: string = res.data;
       // setMouth(res.data);
       // setSv(svgStr.replace("{{colorCode}}", "#ff9473"));
-      // var parser = new DOMParser();
-      // var doc = parser.parseFromString(sv, "image/svg+xml");
-      // const s = doc.firstChild as SVGElement;
-      // console.debug(s.);
-      // const a = new PIXI.ImageResource(
-      //   "https://mii-studio.akamaized.net/editor/1/webp/1024/2/6/5/e12f2403f.webp",
-      //   { crossorigin: true }
-      // );
-      // const t = PIXI.BaseTexture.from(a.source);
-      // console.debug(a.source);
-      // setTex(PIXI.Texture.from(a.source));
-      // const e = PIXI.Texture.from(
-      //   "https://mii-studio.akamaized.net/editor/1/webp/1024/a/6/8/4f169637a.webp"
-      // );
-      // setEyeTex(e);
     })();
   }, []);
 
@@ -130,18 +101,10 @@ function App() {
         min={-50}
         max={50}
         setLocation={(value) => {
-          let color = Math.ceil(16777215 * Math.random()).toString(16);
-          let length = color.length;
-          while (length < 6) {
-            color = "0" + color;
-            length++;
-          }
-          setSv(mouth.replace("{{colorCode}}", "#" + color));
-          // const scale = value / 10;
-          // setFaceLocation((prevState) => ({
-          //   ...prevState,
-          //   y: CANVAS_HEIGHT / 2 + value,
-          // }));
+          setFaceLocation((prevState) => ({
+            ...prevState,
+            y: CANVAS_HEIGHT / 2 + value,
+          }));
         }}
       />
       <Slider
@@ -269,7 +232,6 @@ function App() {
               console.debug("her");
               ctx.drawImage(canv, 0, 0, canv.width, canv.height);
               var base64 = canv2.toDataURL();
-              const a = canv2.toDataURL("image/png");
               // console.debug(base64);
               // console.debug(a);
               const anchor: HTMLAnchorElement = document.createElement("a");
@@ -296,7 +258,7 @@ function App() {
           }}
         >
           <Sprite
-            texture={tex}
+            texture={PIXI.Texture.from(face)}
             anchor={0.5}
             x={faceLocation.x}
             y={faceLocation.y}
@@ -311,15 +273,13 @@ function App() {
               scale={faceLocation.scale}
             />
           )} */}
-          {!!sv && (
-            <Sprite
-              texture={PIXI.Texture.from(sv)}
-              anchor={0.5}
-              x={mouthLocation.x}
-              y={faceLocation.y + mouthLocation.y}
-              scale={mouthLocation.scale}
-            />
-          )}
+          <Sprite
+            texture={PIXI.Texture.from(mouth)}
+            anchor={0.5}
+            x={mouthLocation.x}
+            y={faceLocation.y + mouthLocation.y}
+            scale={mouthLocation.scale}
+          />
           {/* <Sprite
             texture={PIXI.Texture.from(
               "https://mii-studio.akamaized.net/editor/1/webp/1024/1/4/c/6350bfba4.webp"
@@ -330,7 +290,7 @@ function App() {
             scale={mouthLocation.scale}
           /> */}
           {/* s3画像は普通にやるとcorsで弾かれる */}
-          <Sprite
+          {/* <Sprite
             texture={PIXI.Texture.from(
               "https://docs.aws.amazon.com/ja_jp/sdk-for-javascript/v3/developer-guide/images/s3-photo-album-example.png"
             )}
@@ -338,25 +298,28 @@ function App() {
             x={noseLocation.x}
             y={faceLocation.y + noseLocation.y}
             scale={noseLocation.scale}
+          /> */}
+          <Sprite
+            texture={PIXI.Texture.from(nose)}
+            anchor={0.5}
+            x={noseLocation.x}
+            y={faceLocation.y + noseLocation.y}
+            scale={noseLocation.scale}
           />
-          {eyetex && (
-            <>
-              <Sprite
-                texture={eyetex}
-                anchor={centerAnchor}
-                x={leftEyeLocation.x}
-                y={faceLocation.y + leftEyeLocation.y}
-                scale={{ x: -leftEyeLocation.scale, y: leftEyeLocation.scale }}
-              />
-              <Sprite
-                texture={eyetex}
-                anchor={centerAnchor}
-                x={rightEyeLocation.x}
-                y={faceLocation.y + rightEyeLocation.y}
-                scale={rightEyeLocation.scale}
-              />
-            </>
-          )}
+          <Sprite
+            texture={PIXI.Texture.from(eye)}
+            anchor={centerAnchor}
+            x={leftEyeLocation.x}
+            y={faceLocation.y + leftEyeLocation.y}
+            scale={{ x: -leftEyeLocation.scale, y: leftEyeLocation.scale }}
+          />
+          <Sprite
+            texture={PIXI.Texture.from(eye)}
+            anchor={centerAnchor}
+            x={rightEyeLocation.x}
+            y={faceLocation.y + rightEyeLocation.y}
+            scale={rightEyeLocation.scale}
+          />
         </Stage>
       </div>
       <canvas id="aa" />
